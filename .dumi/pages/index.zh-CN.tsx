acab96aec3e841eb804eaf242b8442137ddb475d
liuyib <1656081615@qq.com>
2025-07-15 00:43:35 +0800

    fix: docs build bundle has no index.html

---
+++ .dumi/pages/index.zh-CN.tsx
@@ -0,0 +1,12 @@
+/**
+ * sidebar: false
+ */
+
+import { HomeBaseLayout } from '@ahooks.js/dumi-theme-antd';
+import React from 'react';
+
+const Index = () => {
+  return <HomeBaseLayout />;
+};
+
+export default Index;
